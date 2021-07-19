d7ebc128151bcd9fbc428e31f2ab52bb647200b3
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-07-19 09:48:41 -0300

    :sparkles: feat - update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,11 @@ public class Program {
         System.out.println("\n--- Test 4: Seller: insert ---");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
+
+        System.out.println("\n--- Test 5: Seller: update ---");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
